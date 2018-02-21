5c59e111fb2ee4f93179f2ab93a3ebe892c532af
Boon <low_boon_hean@ida-gds.com>
2018-02-21 08:39:49 +0800

    change password to percentage encoding

--- start_app_server.sh
+++ start_app_server.sh
@@ -3,7 +3,7 @@
 
 # READ SECRET!
 export MONGODB_CREDENTIALS=
-mongodb://boonzilla:P@ssw0rd@ds143738.mlab.com:43738/boonzilla
+mongodb://boonzilla:P%40ssw0rd@ds143738.mlab.com:43738/boonzilla
 
 # finally, run the app in dev mode...
 npm run dev
