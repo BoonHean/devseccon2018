38412707df74ba0f652d3336f9f92268f06a143d
Boon <low_boon_hean@ida-gds.com>
2018-02-21 08:53:39 +0800

    remove secret

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://boonzilla:P%40ssw0rd@ds143738.mlab.com:43738/boonzilla
+#export MONGODB_CREDENTIALS=mongodb://boonzilla:P%40ssw0rd@ds143738.mlab.com:43738/boonzilla
 
 # finally, run the app in dev mode...
 npm run dev
